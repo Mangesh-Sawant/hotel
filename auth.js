e3743db9dc4cd5cbfbda2d29ca3e7924bf365123
mangesh sawant <mangesh.sawant@ilmtec.in>
2024-10-05 20:05:37 +0530

    add auth.js for autentication use passport

---
+++ auth.js
@@ -0,0 +1,22 @@
+const passport = require('passport');
+const Person = require('./models/Person')
+const LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(async (username, password, done) => {
+    try {
+        console.log('Received Credentails', username, password);
+        const user = await Person.findOne({ username: username });
+        if (!user)
+            return done(null, false, { message: 'Incorrect User name.' });
+        const isPasswordMatch = user.password === password ? true : false;
+        if (isPasswordMatch) {
+            return done(null, user);
+        } else {
+            return done(null, false, { message: 'Incorrect Password.' })
+        }
+    } catch (err) {
+        return done(err)
+    }
+}));
+
+module.exports = passport;
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors'); // Import the CORS middleware
 const db = require('./db');
-require('dotenv').config(); 
+const passport = require('./auth');
+require('dotenv').config();
 const personRoutes = require('./routes/personRoutes');
 const menuRoutes = require('./routes/menuRoutes');
 
@@ -15,6 +16,10 @@ app.use(express.json()); // You can replace bodyParser.json() with express.json(
 // Configure CORS to allow all origins
 app.use(cors());
 
+//auth
+
+app.use(passport.initialize());
+
 // If you prefer to allow specific origins, use the following configuration instead:
 // const corsOptions = {
 //   origin: ['https://your-frontend-domain.com', 'http://localhost:3000'], // Replace with your frontend URLs
@@ -24,11 +29,13 @@ app.use(cors());
 // };
 // app.use(cors(corsOptions));
 
+const localAuthMiddleware = passport.authenticate('local', { session: false });
+
 app.get('/', (req, res) => {
   res.send('Welcome to your Hotel');
 });
 
-app.use('/person', personRoutes);
+app.use('/person',localAuthMiddleware, personRoutes);
 app.use('/menu', menuRoutes);
 
 app.listen(PORT, () => {
