a2a2b92a09f703592ef6f6aa134c9ffaa0deaf4e
mangesh sawant <mangesh.sawant@ilmtec.in>
2024-10-05 21:21:46 +0530

    add decript password store in database using bcrypt

--- auth.js
+++ auth.js
@@ -8,7 +8,7 @@ passport.use(new LocalStrategy(async (username, password, done) => {
         const user = await Person.findOne({ username: username });
         if (!user)
             return done(null, false, { message: 'Incorrect User name.' });
-        const isPasswordMatch = user.password === password ? true : false;
+        const isPasswordMatch = await user.comparePassword(password);
         if (isPasswordMatch) {
             return done(null, user);
         } else {
--- models/Person.js
+++ models/Person.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const personSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -38,6 +39,33 @@ const personSchema = new mongoose.Schema({
         required: true
     }
 });
+
+// bycript
+personSchema.pre('save', async function (next) {
+    const person = this;
+    //other than passprost opration
+    if (!person.isModified('password')) return next();
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(person.password, salt);
+        person.password = hashedPassword;
+        next();
+    } catch (err) {
+        return next(err);
+    }
+});
+
+personSchema.methods.comparePassword = async function (candidatePassword) {
+    try {
+        const isMatch = await bcrypt.compare(candidatePassword, this.password);
+        return isMatch;
+    } catch (err) {
+        throw err;
+    }
+};
+
+
+
 
 //create person model 
 
